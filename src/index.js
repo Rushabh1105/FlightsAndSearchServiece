22523fa1812242730b3c2aa851f40002803cde50
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2022-12-25 16:44:40 +0530

    setup for sequelize and mysql2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const {PORT} = require("./config/serverConfig")
 
 const setupAndServer = async () => {
     //Create the express object
     const app = express();
-    //const port = 3000;
 
+    //Middle wares
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    //start server
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     })
