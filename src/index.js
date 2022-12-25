d88ccbdbeb8008cfc21d2da7928f92202e177caf
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2022-12-25 18:53:22 +0530

    added city-repository layer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const {PORT} = require("./config/serverConfig")
+const {PORT} = require("./config/serverConfig");
+const {City} = require("../src/models/index");
 
 const setupAndServer = async () => {
     //Create the express object
@@ -12,8 +13,9 @@ const setupAndServer = async () => {
     app.use(bodyParser.urlencoded({extended : true}));
 
     //start server
-    app.listen(PORT, () => {
+    app.listen(PORT,async () => {
         console.log(`Server started at ${PORT}`);
+        //await City.create({name : "New Delhi"});
     })
 
 }
