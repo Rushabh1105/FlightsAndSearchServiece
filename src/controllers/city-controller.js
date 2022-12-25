0f08114bc28ca26aefd2d2ef3d17d8e0916ef40a
Rushabh1105 <rushabh.sakharkar1998@gmail.com>
2022-12-26 02:16:20 +0530

    added city-controller layer

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,35 +1,80 @@
 const { CityServiece } = require("../servieces/index");
 
+const cityServiece = new CityServiece;
 
-const createCity = async () => {
+const createCity = async (req, res) => {
     try {
-        
+        const response = await cityServiece.createCity(req.body);
+        return res.status(201).json({
+            success : true,
+            message : "created a city",
+            data : response,
+            err : {},
+        });
+
     } catch (error) {
-        throw {error}
+        console.log("error at controller level");
+        return res.status(500).json({
+            success : false,
+            message : "something went wrong",
+            err : error,
+        })
     }
 }
 
-const deleteCity = async () => {
+const deleteCity = async (req, res) => {
     try {
-        
+        const response = await cityServiece.deleteCity(req.params.id);
+        return res.status(200).json({
+            success : true,
+            message : "deleted a city",
+            data : response,
+            err : {},
+        });
     } catch (error) {
-        throw {error}
+        console.log("error at controller level");
+        return res.status(500).json({
+            success : false,
+            message : "something went wrong",
+            err : error,
+        })
     }
 }
 
-const updateCity = async () => {
+const updateCity = async (req, res) => {
     try {
-        
+        const response = await cityServiece.updateCity(req.params.id, req.body);
+        return res.status(200).json({
+            success : true,
+            message : "updated a city",
+            data : response,
+            err : {},
+        });
     } catch (error) {
-        throw {error}
+        console.log("error at controller level");
+        return res.status(500).json({
+            success : false,
+            message : "something went wrong",
+            err : error,
+        })
     }
 }
 
-const getCity = async () => {
+const getCity = async (req, res) => {
     try {
-        
+        const response = await cityServiece.getCity(req.params.id);
+        return res.status(200).json({
+            success : true,
+            message : "fetched a city",
+            data : response,
+            err : {},
+        });
     } catch (error) {
-        throw {error}
+        console.log("error at controller level");
+        return res.status(500).json({
+            success : false,
+            message : "something went wrong",
+        })
     }
 }
 
